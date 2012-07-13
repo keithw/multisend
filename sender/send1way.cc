2743f769585b8a7d8f3e0125efd550d16f2bbdf7
Keith Winstein <keithw@mit.edu>
2012-07-13 05:18:40 -0400

    Do measurement

--- sender/send1way.cc
+++ sender/send1way.cc
@@ -55,11 +55,14 @@ int main( void )
   
   for ( int i = 0; i < 3; i++ ) {
     target.push_back( get_nat_addr( lte_socket[ i ], ethernet_address, ethernet_socket ) );
-    printf( "LTE %d = %s\n", i, target.back().str().c_str() );
+    fprintf( stderr, "LTE %d = %s\n", i, target.back().str().c_str() );
   }
 
+  /* Make dummy address */
+  Socket::Address dummy_address( "220.181.111.147", 9004 );
+
   /* Send and get timestamps */
-  const uint64_t NUM_PACKETS = 10000;
+  const uint64_t NUM_PACKETS = 1400000;
 
   struct pollfd pollfds[ 3 ];
   for ( int i = 0; i < 3; i++ ) {
@@ -67,24 +70,86 @@ int main( void )
     pollfds[ i ].events = POLLIN;
   }
 
-  const int MAX_OUTSTANDING = 30;
-  vector<int> num_outstanding( 3, 0 );
+  const int MAX_OUTSTANDING = 1000;
+  vector<int> num_outstanding( 3 );
 
-  vector<uint64_t> sent_times( NUM_PACKETS );
+  struct packetstat {
+    uint64_t sent_time;
+    int dest;
+    bool received;
+  };
+
+  vector<struct packetstat> packetstats( NUM_PACKETS );
+  uint64_t next_unacked = 0;
 
   uint64_t packets_sent = 0;
 
+  uint64_t base_time = Socket::timestamp();
+
+  const uint64_t TIMEOUT = 500 * 1000 * 1000; /* 1 second */
+
+  uint64_t last_time = base_time;
+
+  vector<int> last_sec_count( 3 );
+
+  int numextra = 0;
+
   while ( packets_sent < NUM_PACKETS ) {
+    uint64_t the_time = Socket::timestamp();
+
+    if ( (the_time / 100000000) != (last_time / 100000000) ) {
+      if ( (the_time / 20000000000) != (last_time / 20000000000) ) {
+	numextra = (numextra + 1) % 3;
+	fprintf( stderr, "%ld FLIP %d\n", (the_time - base_time) / 10000000, numextra );
+      }
+
+      last_time = the_time;
+      printf( "%ld %d %d %d\n", (the_time - base_time) / 100000000, last_sec_count[ 0 ], last_sec_count[ 1 ], last_sec_count[ 2 ] );
+      fflush( NULL );
+      for ( int i = 0; i < 3; i++ ) {
+	last_sec_count[ i ] = 0;
+	char *seq_encoded = (char *)&packets_sent;
+	ethernet_socket.send( Socket::Packet( target[ i ], string( seq_encoded, 51 ) ) );
+	packetstats[ packets_sent ].sent_time = Socket::timestamp();
+	packetstats[ packets_sent ].dest = i;
+	packets_sent++;
+	num_outstanding[ i ]++;
+      }
+    }
+
+    /* update next_unacked */
+    while ( packetstats[ next_unacked ].received ) {
+      next_unacked++;
+      /* possible timeout */
+      if ( !packetstats[ next_unacked ].received ) {
+	if ( the_time - packetstats[ next_unacked ].sent_time > TIMEOUT ) {
+	  fprintf( stderr, "Timeout on %ld (sent to %d)\n", next_unacked, packetstats[ next_unacked ].dest );
+	  packetstats[ next_unacked ].received = true;
+	  num_outstanding[ packetstats[ next_unacked ].dest ]--;
+	}
+      }
+    }
+
     /* send packet if any receivers can be replenished */
-    for ( int i = 0; i < 3; i++ ) {
+    for ( int i = 0; i < 1 + numextra; i++ ) {
       if ( num_outstanding[ i ] < MAX_OUTSTANDING ) {
 	int packets_to_send = MAX_OUTSTANDING - num_outstanding[ i ];
 	for ( int pnum = 0; pnum < packets_to_send; pnum++ ) {
 	  char *seq_encoded = (char *)&packets_sent;
-	  ethernet_socket.send( Socket::Packet( target[ i ], string( seq_encoded, sizeof( packets_sent ) ) ) );
-	  sent_times[ packets_sent ] = Socket::timestamp();
+	  ethernet_socket.send( Socket::Packet( target[ i ], string( seq_encoded, 51 ) ) );
+	  packetstats[ packets_sent ].sent_time = Socket::timestamp();
+	  packetstats[ packets_sent ].dest = i;
 	  packets_sent++;
 	  num_outstanding[ i ]++;
+
+	  if ( i == 0 ) {
+	    if ( numextra < 2 ) {
+	      ethernet_socket.send( Socket::Packet( dummy_address, string( seq_encoded, 51 ) ) );
+	    }
+	    if ( numextra < 1 ) {
+	      ethernet_socket.send( Socket::Packet( dummy_address, string( seq_encoded, 51 ) ) );
+	    }
+	  }
 	}
       }
     }
@@ -103,10 +168,22 @@ int main( void )
 
       if ( pollfds[ i ].revents & POLLIN ) {
 	Socket::Packet rec = lte_socket[ i ].recv();
-	assert ( rec.payload.size() == sizeof( packets_sent ) );
+	//	assert ( rec.payload.size() == sizeof( packets_sent ) );
 	uint64_t seq = *(uint64_t *)rec.payload.data();
-	fprintf( stderr, "%d %ld %ld %ld\n", i, seq, sent_times[ seq ], rec.timestamp );
-	num_outstanding[ i ]--;
+	last_sec_count[ i ]++;
+	assert( packetstats[ seq ].dest == i );
+	if ( !packetstats[ seq ].received ) {
+	  packetstats[ seq ].received = true;
+	  num_outstanding[ i ]--;
+	} else {
+	  fprintf( stderr, "Packet %ld arrived after timeout!\n", seq );
+	}
+
+	/*
+	double sent_ts = double(packetstats[ seq ].sent_time - base_time) / 1000000.0;
+	double recv_ts = double(rec.timestamp - base_time) / 1000000.0;
+	*/
+	//	printf( "%d %ld %.8f %.8f delay=%.8f %d oldest-queued=%ld\n", i, seq, sent_ts, recv_ts, recv_ts - sent_ts, num_outstanding[ i ], packets_sent - next_unacked );
       }
     }
   }
