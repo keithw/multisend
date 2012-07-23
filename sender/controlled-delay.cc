0a5875e2ad1bf240827d8cf345f0139f9a816334
Keith Winstein <keithw@mit.edu>
2012-07-23 13:31:29 -0400

    Does a pretty good job. May need to use interarrival times.

--- sender/controlled-delay.cc
+++ sender/controlled-delay.cc
@@ -54,56 +54,66 @@ int main( void )
   Socket::Address target( get_nat_addr( lte_socket, ethernet_address, ethernet_socket ) );
   fprintf( stderr, "LTE = %s\n", target.str().c_str() );
 
-  RateEstimate rate_estimator( 30.0, 500 );
+  RateEstimate rate_estimator( 1.0, 1000 );
   
   const unsigned int PACKET_SIZE = 600;
   unsigned int packets_outstanding = 0;
   unsigned int packets_sent = 0;
 
   const double QUEUE_DURATION_TARGET = 2.0;
-  const double STEERING_TIME = 5.0;
+  const double STEERING_TIME = 1.0;
 
   int my_pid = (int) getpid();
 
   uint64_t next_transmission = Socket::timestamp();
+  uint64_t last_transmission = next_transmission;
 
   while ( 1 ) {
     fflush( NULL );
     
     uint64_t now = Socket::timestamp();
 
+    /* schedule next packet transmission */
+
+    /* Q: When will queue hit our target? */
+    /* Step 1: Estimate queue size in ms */
+
+    double queue_duration_estimate = (double) packets_outstanding / rate_estimator.get_rate(); /* in seconds */
+
+    /* Step 2: At what rate, will queue duration hit the target exactly STEERING_TIME seconds in the future? */
+
+    /* We want to account for this much difference over the next STEERING_TIME seconds */
+    double queue_duration_difference = QUEUE_DURATION_TARGET - queue_duration_estimate;
+
+    double extra_packets_needed = queue_duration_difference * rate_estimator.get_rate() + STEERING_TIME * rate_estimator.get_rate();
+    double extra_packet_rate = extra_packets_needed / STEERING_TIME; /* packets per second */
+
+    uint64_t interpacket_delay = 1.e9 / extra_packet_rate;
+
+    if ( extra_packet_rate > 0 ) {
+      /* schedule next transmission */
+      next_transmission = last_transmission + interpacket_delay;
+    } else {
+      next_transmission = uint64_t( -1 ); /* never */
+    }
+
+    //    printf( "Queue duration estimate: %f\n", queue_duration_estimate );
+
     if ( next_transmission <= now ) {
       Payload outgoing;
       outgoing.sequence_number = packets_sent++;
       outgoing.sent_timestamp = Socket::timestamp();
       outgoing.process_id = my_pid;
-      printf( "SENDING...\n" );
       ethernet_socket.send( Socket::Packet( target, outgoing.str( PACKET_SIZE ) ) );
       packets_outstanding++;
+      last_transmission = outgoing.sent_timestamp;
+    }
 
-      /* schedule next packet transmission */
-
-      /* Q: When will queue hit our target? */
-      /* Step 1: Estimate queue size in ms */
-
-      double queue_duration_estimate = (double) packets_outstanding / rate_estimator.get_rate(); /* in seconds */
-
-      /* Step 2: At what rate, will queue duration hit the target exactly STEERING_TIME seconds in the future? */
-
-      /* We want to account for this much difference over the next STEERING_TIME seconds */
-      double queue_duration_difference = QUEUE_DURATION_TARGET - queue_duration_estimate;
-
-      double extra_packets_needed = queue_duration_difference * rate_estimator.get_rate() + STEERING_TIME * rate_estimator.get_rate();
-      double extra_packet_rate = extra_packets_needed / STEERING_TIME; /* packets per second */
-
-      if ( extra_packet_rate > 0 ) {
-	/* schedule next transmission */
-	next_transmission = now + (uint64_t) (1.e9 / extra_packet_rate);
-      } else {
-	next_transmission = uint64_t( -1 ); /* never */
-      }
-
-      printf( "Queue duration estimate: %f\n", queue_duration_estimate );
+    if ( extra_packet_rate > 0 ) {
+      /* schedule next transmission */
+      next_transmission = last_transmission + interpacket_delay;
+    } else {
+      next_transmission = uint64_t( -1 ); /* never */
     }
 
     /* wait for incoming packet OR expiry of timer */
@@ -111,8 +121,6 @@ int main( void )
     struct pollfd poll_fds[ 1 ];
     poll_fds[ 0 ].fd = lte_socket.get_sock();
     poll_fds[ 0 ].events = POLLIN;
-
-    assert( next_transmission >= now );
 
     if ( next_transmission == uint64_t( -1 ) ) {
       ppoll( poll_fds, 1, NULL, NULL );
@@ -130,9 +138,11 @@ int main( void )
       contents->recv_timestamp = incoming.timestamp;
       if ( contents->process_id == my_pid ) {
 	rate_estimator.add_packet( *contents );
-	printf( "Rate estimate: %f packets per second [%d sent]\n", rate_estimator.get_rate(), packets_sent );
-	printf( "Delay: %f ms\n", (contents->recv_timestamp - contents->sent_timestamp) / 1.0e6 );
 	packets_outstanding--;
+	printf( "delay = %f recvrate = %f sendrate = %f queueest = %f\n", (contents->recv_timestamp - contents->sent_timestamp) / 1.0e6,
+		rate_estimator.get_rate(),
+		extra_packet_rate,
+		(double) packets_outstanding / rate_estimator.get_rate() );
       }
     }
   }
