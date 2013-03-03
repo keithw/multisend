159d9dea936b081e8250d21f846b7bdbfb3c5840
Keith Winstein <keithw@mit.edu>
2013-03-03 09:20:38 -0500

    Script to run cellsim

---
+++ sender/cellsim-runner.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nohup /home/ubuntu/multisend/sender/cellsim /home/ubuntu/verizon_lte_uplink_slice.ms /home/ubuntu/verizon_lte_downlink_slice.ms 00:00:00:00:00:02 0 LTE-eth0 LTE-eth1 >/tmp/cellsim-stdout 2>/tmp/cellsim-stderr &
